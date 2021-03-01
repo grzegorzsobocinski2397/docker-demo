dbe6b8dbdf2bba744e32850e88cc436f99ee2523
Mateusz Marszalek <mateusz.marszalek@pl.abb.com>
2021-03-01 15:02:25 +0100

    Add backend

---
+++ back-end/Controllers/ValueController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string variableName = "MESSAGE")
+        {
+            return Environment.GetEnvironmentVariable(variableName);
+        }
+    }
+}
